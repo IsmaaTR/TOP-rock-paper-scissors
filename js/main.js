c36f7fc9cbf90013b9ea8ddc98fe6893f050ec6c
Isma <112580032+IsmaaTR@users.noreply.github.com>
2025-02-09 12:11:14 +0100

    Finished getComputerChoice function.
    Implemented the functionality to generate randomly rock/paper/scissors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 function getComputerChoice() {
-    console.info("Computer choice working!");
+    // Generate a random number between 0 and 2 
+    const number = getRandomInt(3);
+
+    // Depending on the number, return 'rock', 'paper', or 'scissors'
+    switch (number) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
